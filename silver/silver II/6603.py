9a6e02e76e6c685825ab0ab2085e25d6e1bd9aa5
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-01-20 17:08:41 +0900

    Create 6603.py
    백준 6603번 로또
    걸린 시간: 32ms

---
+++ silver/silver II/6603.py
@@ -0,0 +1,18 @@
+# 백준 6603번 로 실버 2
+
+import sys
+from itertools import combinations
+
+T = []
+while True:
+  num_list = list(map(int, sys.stdin.readline().split()))
+  if num_list[0] == 0:
+    break
+  T.append(num_list)
+
+for t in T:
+  nCr = combinations(t[1:t[0] + 1], 6)
+
+  for i in list(nCr):
+    print(*i)
+  print()
