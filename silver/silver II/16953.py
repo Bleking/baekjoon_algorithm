c3df14a1eab13b84a39cb315ff8101bf84d688fb
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-01-27 13:24:58 +0900

    Create 16953.py
    백준 16953번 A → B
    걸린 시간: 32ms

---
+++ silver/silver II/16953.py
@@ -0,0 +1,22 @@
+# 백준 16953번 	A → B 실버 2
+import sys
+
+A, B = map(int, sys.stdin.readline().split())
+
+count = 1
+while B != A:
+  if B < A:
+    count = -1
+    break
+  
+  if B % 2 == 0:
+    B = B // 2
+  elif B % 10 == 1:
+    B = B // 10
+  else:
+    count = -1
+    break
+  
+  count += 1
+
+print(count)
