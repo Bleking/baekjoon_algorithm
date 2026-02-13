b69e192db366f43fdc5b18cc52af0d8453352ae9
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-02-13 19:42:12 +0900

    Create 2467.py
    백준 2467번 용액
    걸린 시간: 112ms

---
+++ gold/gold V/2467.py
@@ -0,0 +1,32 @@
+# 백준 2467번 용액 골드 5
+
+import sys
+
+N = int(sys.stdin.readline())
+liquid = list(map(int, sys.stdin.readline().split()))
+
+liquid.sort()
+
+pl, pr = 0, N-1
+ans_left, ans_right = pl, pr
+
+answer = abs(liquid[pl] + liquid[pr])
+
+while pl < pr:
+  sum = liquid[pl] + liquid[pr]
+
+  if abs(sum) < answer:
+    answer = abs(sum)
+
+    ans_left = pl
+    ans_right = pr
+
+    if answer == 0:
+      break
+
+  if sum < 0:
+    pl += 1
+  else:
+    pr -= 1
+
+print(liquid[ans_left], liquid[ans_right])
