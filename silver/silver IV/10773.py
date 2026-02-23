969ef609a3c7bffa50cc2694cb380340a8d92059
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-02-23 11:16:09 +0900

    Create 10773.py
    백준 10773번 제로
    걸린 시간: 72ms

---
+++ silver/silver IV/10773.py
@@ -0,0 +1,15 @@
+# 백준 10773번 제로 실버 4
+
+import sys
+
+K = int(sys.stdin.readline())
+
+stack = []
+for i in range(K):
+  num = int(sys.stdin.readline())
+  if num == 0:
+    stack.pop()
+  else:
+    stack.append(num)
+
+print(sum(stack))
