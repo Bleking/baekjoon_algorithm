7b291f9d5ca3b5ef47cdb89b2eb057a5fda3cdc1
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-03-03 18:51:31 +0900

    Create 13398.py
    백준 13398번 연속합 2
    걸린 시간: 116ms

---
+++ gold/gold IV/13398.py
@@ -0,0 +1,15 @@
+# 백준 13398번 연속합 2 골드 4
+
+import sys
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+
+dp = [[0] * N for _ in range(2)]
+dp[0][0], dp[1][0] = arr[0], arr[0]
+
+for i in range(1, N):
+  dp[0][i] = max(dp[0][i-1] + arr[i], arr[i])
+  dp[1][i] = max(dp[0][i-1], dp[1][i-1] + arr[i])
+
+print(max(max(dp[0]), max(dp[1])))
