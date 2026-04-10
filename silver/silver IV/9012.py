6b95d8506fb816a6acf8d05a36df9c480daee82e
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-04-10 13:20:17 +0900

    Create 9012.py
    백준 9012번 괄호
    걸린 시간: 36ms

---
+++ silver/silver IV/9012.py
@@ -0,0 +1,23 @@
+# 백준 9012번 괄호 실버 4
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for t in range(T):
+  S = sys.stdin.readline().strip()
+  stack = []
+
+  for s in S:
+    if s == '(':
+      stack.append(s)
+    elif s == ')':
+      if stack and stack[-1] == '(':
+        stack.pop()
+      else:
+        stack.append(s)
+  
+  if stack:
+    print('NO')
+  else:
+    print('YES')
