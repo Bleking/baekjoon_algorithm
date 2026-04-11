7b3c9dbcbae8537175dcc8dead7cab4e1b521f55
Jiwon Ha <79359994+Bleking@users.noreply.github.com>
2026-04-11 12:55:31 +0900

    [Gold V] Title: 탑, Time: 572 ms, Memory: 115920 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,22 @@
+import sys
+
+N = int(sys.stdin.readline())
+towers = list(map(int, sys.stdin.readline().split()))
+
+stack = []
+answer = []
+
+for i in range(N):
+  while stack:
+    if stack[-1][1] < towers[i]:
+      stack.pop()
+    else:
+      answer.append(stack[-1][0])
+      break
+
+  if not stack:
+    answer.append(0)
+
+  stack.append((i+1, towers[i]))
+
+print(*answer)
